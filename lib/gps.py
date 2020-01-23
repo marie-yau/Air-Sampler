ac55731cda7bc9e15748199f992b1112f0b490c6
Marie Yau <marie.dohnalova21@gmail.com>
2020-01-22 21:58:20 -0800

    new changes

--- lib/gps.py
+++ lib/gps.py
@@ -9,26 +9,26 @@ class GPS():
     def __init__(self):
         self._gpgga_info = "$GPGGA,"
         self._ser = serial.Serial("/dev/ttyAMA0")
-        self._gpgga_buffer = 0
-        self._nmea_buffer = 0
+        self.get_current_coordinates()
 
-
-    def get_GPS_nmea_info(self):
+    def read_GPS_information(self):
         while True:
             data_from_module = (str)(self._ser.readline())
-            gpgga_data_from_module = data_from_module.find(self.gpgga_info)
-            if gpgga_data_from_module > 0:
-                self._gpgga_buffer = data_from_module.split("$GPGGA,", 1)[1]
-                nmea_buffer = (self._gpgga_buffer.split(','))
+            if data_from_module[0:7] == self._gpgga_info:
+                gpgga_buffer = data_from_module.split(self._gpgga_info, 1)[1]
+                nmea_buffer = (gpgga_buffer.split(','))
                 self._nmea_time = nmea_buffer[0]
                 self._nmea_latitude = nmea_buffer[1]
+                self.latitude_direction = nmea_buffer[2]
                 self._nmea_longitude = nmea_buffer[3]
-                self._nmea_latitude, self.nmea_longitude
-                break
+		        self.longitude_direction = nmea_buffer[4]
+		        break
 
-
-    def convert_nmea_to_degrees(self):
-        nmea_value = float(self._nmea_value)
+    def _convert_nmea_to_degrees(self, nmea_value):
+        try:
+            nmea_value = float(nmea_value)
+        except(TypeError, ValueError):
+            nmea_value = 0
         decimal_value = nmea_value / 100.00
         degrees = int(decimal_value)
         minutes = (decimal_value - int(decimal_value)) / 60
@@ -36,21 +36,21 @@ class GPS():
         position = "%.4f" % (position)
         return position
 
-
-    def get_latitude_and_longitude_in_degrees(self):
-        nmea_latitude, nmea_longitude = self.get_GPS_nmea_info()
-        degrees_latitude = self.convert_nmea_to_degrees(nmea_latitude)
-        degrees_longitude = self.convert_nmea_to_degrees(nmea_longitude)
-        return degrees_latitude, degrees_longitude
+    def get_coordinates(self):
+        self.latitude_degrees = self._convert_nmea_to_degrees(self._nmea_latitude)
+        self.longitude_degrees = self._convert_nmea_to_degrees(self._nmea_longitude)
+        return self.latitude_degrees, self.latitude_direction, self.longitude_degrees, self.longitude_direction
 
     def get_time(self):
-        self.time_formatted = self._nmea_time[0:2] + ":" + self._nmea_time[2:4] + ":" + self._nmea_time[4:6]
-        return self.time_formatted
+        self.time = self._nmea_time[0:2] + ":" + self._nmea_time[2:4] + ":" + self._nmea_time[4:6]
+        return self.time
 
-
-    def __test__(self):
-
+    def print_coordinates_and_time(self):
+        print("Time: ", self.time)
+        print("Latitude: ", self.latitude_degrees, " ", self.latitude_direction)
+        print("Longitude: ", self.longitude_degrees, " ", self.longitude_direction)
 
 if __name__ == "__main__":
-    gps = GPS()
-    GPS.__test__()
+    myGPS = GPS()
+    myGPS.read_GPS_information()
+    myGPS.print_coordinates_and_time()
