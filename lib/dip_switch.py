f220f9deb226205085b18c0bf89e70e5fa0df4b4
Marie Yau <marie.dohnalova21@gmail.com>
2020-01-25 09:58:29 -0800

    corrected dip switch and added comments

--- lib/dip_switch.py
+++ lib/dip_switch.py
@@ -3,6 +3,7 @@ Read dual inline package (DIP) switches.
 """
 
 import RPi.GPIO as GPIO
+import settings
 
 class DIPSwitch():
     """
@@ -11,74 +12,91 @@ class DIPSwitch():
 
     def __init__(self, dip_switch_pin_numbers, mode):
         """
-        Set board numbering mode to either "BCM" or "BOARD" and initialize maximum number of Pi's pins to default value
-        40.
         :param dip_switch_pin_numbers: List of at least one integer that represent unique valid pin numbers of the Pi
                 that the DIP switch is connected to. The pins in the list are ordered low-to-high.
-        :param mode: string that represents the board numbering mode. Must be either "BCM" or "BOARD".
+                E.g. DIP switch has two switches. Switch with physical number 1 is connected to GPIO 26 and switch with
+                number 2 to GPIO 19, so the `dip_switch_pin_numbers` is equal to [26,19].
+        :param mode: string that represents a board numbering mode. Must be either "BCM" or "BOARD".
         """
-        self._set_mode(mode)
-        self.set_max_number_of_pins(40)
-        self._set_dip_switch_pin_numbers(dip_switch_pin_numbers, self.mode)
+        # set board numbering mode
+        settings.set_board_numbering_mode(mode)
+        self._mode = mode
+        # check if `dip_switch_pin_numbers` is list of valid pin numbers
+        self._set_dip_switch_pin_numbers(dip_switch_pin_numbers)
 
-    def _set_mode(self, mode):
-        assert(mode == "BCM" or mode == "BOARD")
-        self.mode = mode
-        if self.mode == "BCM":
-            GPIO.setmode(GPIO.BCM)
-        elif self.mode == "BOARD":
-            GPIO.setmode(GPIO.BOARD)
-
-    def _set_dip_switch_pin_numbers(self, dip_switch_pin_numbers, mode):
+    def _set_dip_switch_pin_numbers(self, dip_switch_pin_numbers):
         """
         Check if `dip_switch_pin_numbers` is a list of valid pin numbers and set `self.dip_switch_pin_numbers`.
-        `dip_switch_pin_numbers` must satisfy the following conditions:
-        + contains at least one integer
-        + all integers in the list are greater than 0 and at most equal to `max_number_of_pins`
-        + the list doesn't contain repeated integers
         :param dip_switch_pin_numbers: List of at least one integer that represents the pin numbers of the Pi that the
                 DIP switch is connected to.
+                `dip_switch_pin_numbers` must satisfy the following conditions:
+                + contains at least one integer
+                + must be a list of integers
+                + pin numbers must be physically possible
+                + the list doesn't contain any duplicates
         """
-        # possible GPIO pin numbers in BCM mode
-        pins_in_BCM_mode = [2,3,4,17,27,22,10,9,11,5,6,13,19,26,14,15,18,23,24,25,8,7,12,16,20,21]
-        # check if list isn't empty
+        # check if list contains at least one item
         assert(len(dip_switch_pin_numbers) > 0)
+        # check if all items in the list are unique
+        assert(len(dip_switch_pin_numbers) == len(set(dip_switch_pin_numbers)))
         for pin_number in dip_switch_pin_numbers:
             # check if `pin_number` is integer
             assert(isinstance(pin_number, int))
-            # check if the `pin_number` is physically possible
-            assert((self.mode == "BCM" and pin_number in pins_in_BCM_mode) or (self.mode == "BOARD" and pin_number > 0
-                                                                               and pin_number <= self.dip_switch_pin_numbers))
+            # TODO: Check if the `pin_number` is physically possible.
+            # The BCM pin numbers changed between different versions of Pi (revision 1 and revision 2 of Pi).
+            # Should we ask user for revision number or make check more general so both rev1 and rev2 numberings can pass
+            # at the same time?
+            # assert((self._mode == "BCM" and pin_number >= 0 and pin_number <= 27) or (self._mode == "BOARD" and pin_number >= 1 and pin_number <= 40))
         self.dip_switch_pin_numbers = dip_switch_pin_numbers
 
-    def set_max_number_of_pins(self, max_number_of_pins):
-        """
-        Set maximum number of pins that Pi has. User can add more pins to Pi using port expander.
-        :param max_number_of_pins: maximum number of pins that Pi has
-        """
-        assert(max_number_of_pins > 0)
-        assert (isinstance(max_number_of_pins, int))
-        self.max_number_of_pins = max_number_of_pins
-
     def _read_switch_positions(self):
+        """
+        Read the pin numbers of the Pi that the DIP switch is connected to and set items in the `self.switch_positions`
+        list to "1" if the switch is in on position and to "0" if the switch is in off position. The switch positions
+        in the `self.switch_positions` list correspond to the pin numbers in the `self.dip_switch_pin_numbers` list.
+        E.g. When `self.dip_switch_pin_numbers` is equal to [26,19] and GPIO 26 is off and GPIO 19 is on, the
+        `self.switch_positions` is equal to ["0","1"]
+        """
         # set up pins as inputs
         [GPIO.setup(pin_number, GPIO.IN, pull_up_down = GPIO.PUD_UP) for pin_number in self.dip_switch_pin_numbers]
         self.switch_positions = ["1" if GPIO.input(pin) else "0" for pin in self.dip_switch_pin_numbers]
 
-    def get_switch_position(self, index):
+    def get_switch_position(self, switch_number):
+        """
+        Get position of specified switch in DIP switch.
+        :param switch_number: Integer that represents the physical number of switch in DIP switch. The numbers of switches
+                are written on the DIP switch.
+        :return: "0" if switch with `switch number` is in off position or "1" if switch is in on position.
+        """
         self._read_switch_positions()
-        return self.switch_positions[index]
+        # we have to subtract 1 from `switch_number` because `self.switch_positions` is a list that starts with index 0
+        # while switch numbers on DIP switch start with index 1
+        return self.switch_positions[switch_number - 1]
 
     def get_switch_positions(self):
+        """
+        Get positions of all switches in DIP switch.
+        :return: List of "1"s and "0"s that represent on and off positions of all switches in DIP switch. The switch
+                positions in the `self.switch_positions` list correspond to the pin numbers in the
+                `self.dip_switch_pin_numbers` list.
+        """
         self._read_switch_positions()
         return self.switch_positions
 
     @staticmethod
     def convert_switch_positions_to_decimal_number(switch_positions):
+        """
+        Convert "1" and "0" switch positions in `switch_positions` list to a decimal number.
+        E.g. list ["0","1","1"] is converted to decimal number 6.
+        :param switch_positions: List of "1"s and "0"s that represent switch positions in the DIP switch. The list
+                should be ordered low-to-high (position of switch with the lowest physical number should be in position
+                0 in the `switch_positions` list).
+        :return: Integer that represents a decimal number converted from binary number in form of list of "1" and "0".
+        """
         assert(len(switch_positions) > 0)
         decimal_number = 0
         factor = 1
-        for i in range(8):
+        for i in range(len(switch_positions)):
             assert(switch_positions[i] == "0" or switch_positions[i] == "1")
             digit = int(switch_positions[i])
             decimal_number += digit * factor
@@ -92,10 +110,12 @@ class DIPSwitch():
         GPIO.cleanup(self.dip_switch_pin_numbers)
 
 if __name__ == "__main__":
+    # list GPIOs numbers in BCM mode that the DIP switch is connected to in low-to-high order
     dip_switch_pin_numbers = [26, 19, 13, 6, 5, 21, 20, 16]
     dip_switch = DIPSwitch(dip_switch_pin_numbers, "BCM")
+    # get list of switch positions in the DIP switch
     dip_switch_positions = dip_switch.get_switch_positions()
     decimal_number = dip_switch.convert_switch_positions_to_decimal_number(dip_switch_positions)
     print("DIP switch positions: ", dip_switch.get_switch_positions())
     print("DIP switch positions as decimal number: ", str(decimal_number))
-    print("DIP switch 4th position: ", dip_switch.get_switch_position(3))
+    print("DIP switch 4th position: ", dip_switch.get_switch_position(4))
