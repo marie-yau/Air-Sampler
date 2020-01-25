a0169a2abd57eaacb57e5142a155b172d56b104c
Marie Yau <marie.dohnalova21@gmail.com>
2020-01-25 10:04:07 -0800

    added function for setting board numbering mode

---
+++ lib/settings.py
@@ -0,0 +1,10 @@
+def set_board_numbering_mode(mode):
+    """
+    Set board numbering mode to either "BCM" or "BOARD".
+    :param mode: String that represents a board numbering mode. Must be either "BCM" or "BOARD".
+    """
+    assert (mode == "BCM" or mode == "BOARD")
+    if mode == "BCM":
+        GPIO.setmode(GPIO.BCM)
+    elif mode == "BOARD":
+        GPIO.setmode(GPIO.BOARD)
