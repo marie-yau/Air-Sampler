10521fafb95c1c447cf8c6245189bd7c0f6d1a51
Marie Yau <marie.dohnalova21@gmail.com>
2020-01-26 19:26:30 -0800

    made gps return position object instead of coordinates

--- lib/gps.py
+++ lib/gps.py
@@ -1,24 +1,38 @@
+"""
+Package for GPS module.
+"""
 import serial
+from geographic_position import *
 
 class GPS():
     def __init__(self):
-        self._gpgga_info = "$GPGGA,"
+        self._sentence_identifier = "$GPGGA,"
         self._ser = serial.Serial("/dev/ttyAMA0")
 
     def _read_GPS_information(self):
         while True:
             data_from_module = (str)(self._ser.readline())
-            if data_from_module[0:7] == self._gpgga_info:
-                gpgga_buffer = data_from_module.split(self._gpgga_info, 1)[1]
+            if data_from_module[0:7] == self._sentence_identifier:
+                gpgga_buffer = data_from_module.split(self._sentence_identifier, 1)[1]
                 nmea_buffer = (gpgga_buffer.split(','))
                 self._nmea_time = nmea_buffer[0]
                 self._nmea_latitude = nmea_buffer[1]
                 self.latitude_direction = nmea_buffer[2]
                 self._nmea_longitude = nmea_buffer[3]
-		        self.longitude_direction = nmea_buffer[4]
-		        break
+                self.longitude_direction = nmea_buffer[4]
+                break
 
-    def _convert_nmea_to_degrees(self, nmea_value):
+    def get_geographic_position(self):
+        self._read_GPS_information()
+        self.latitude_degrees = self.convert_nmea_to_degrees(self._nmea_latitude)
+        self.longitude_degrees = self.convert_nmea_to_degrees(self._nmea_longitude)
+        self.time = self.convert_nmea_to_time(self._nmea_time)
+        self.position = GeographicPosition(self.latitude_degrees, self.latitude_direction, self.longitude_degrees, self.longitude_direction, self.time)
+        return self.position
+
+    @staticmethod
+    def convert_nmea_to_degrees(nmea_value):
+        # TODO: `nmea_value` must be either string that is convertable to float number or empty string. Test if `nmea_value` satisfies those conditions.
         try:
             nmea_value = float(nmea_value)
         except(TypeError, ValueError):
@@ -30,25 +44,17 @@ class GPS():
         position = "%.4f" % (position)
         return position
 
-    def get_coordinates(self):
-        self._read_GPS_information()
-        self.latitude_degrees = self._convert_nmea_to_degrees(self._nmea_latitude)
-        self.longitude_degrees = self._convert_nmea_to_degrees(self._nmea_longitude)
-        return self.latitude_degrees, self.latitude_direction, self.longitude_degrees, self.longitude_direction
-
-    def get_time(self):
-        self._read_GPS_information()
-        self.time = self._nmea_time[0:2] + ":" + self._nmea_time[2:4] + ":" + self._nmea_time[4:6]
-        return self.time
-
     @staticmethod
-    def print_coordinates_and_time(time, latitude_degrees, latitude_direction, longitude_degrees, longitude_direction):
-        print("Time: ", time)
-        print("Latitude: ", latitude_degrees, " ", latitude_direction)
-        print("Longitude: ", longitude_degrees, " ", longitude_direction)
+    def convert_nmea_to_time(nmea_time):
+        assert(len(nmea_time) == 6)
+        assert(isinstance(nmea_time, str))
+        hours = nmea_time[0:2]
+        minutes = nmea_time[2:4]
+        seconds = nmea_time[4:6]
+        return time(hours, minutes, seconds)
 
 if __name__ == "__main__":
     myGPS = GPS()
-    latitude_degrees, latitude_direction, longitude_degrees, longitude_direction = myGPS.get_coordinates()
-    time = myGPS.get_time()
-    myGPS.print(time, latitude_degrees, latitude_direction, longitude_degrees, longitude_direction)
+    current_position = myGPS.get_geographic_position()
+    print("Coordinates: ", current_position.get_coordinates())
+    print("Time: ", current_position.get_time())
