dfc2c6260046a62459ba8aef853d25f5f8aac8ac
Marie Yau <marie.dohnalova21@gmail.com>
2020-02-01 10:49:08 -0800

    update class for operating pump

---
+++ lib/pump.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import settings
+import check
+
+class Pump():
+    __slots__ = ["pump_pin_number", "pump_on", "mode"]
+    def __init__(self, pump_pin_number, mode):
+        # set board numbering mode
+        settings.set_board_numbering_mode(mode)
+        self.mode = mode
+        self.set_pump_pin_number(pump_pin_number)
+        self.__pump_set_up()
+
+    def set_pump_pin_number(self, pump_pin_number):
+        assert(check.is_valid_GPIO_pin_number(pump_pin_number, self.mode))
+        self.pump_pin_number = pump_pin_number
+
+    def __pump_set_up(self):
+        GPIO.setup(self.pump_pin_number, GPIO.OUT)
+        self.pump_on = False
+
+    def start_pumping(self):
+        GPIO.output(self.pump_pin_number, 1)
+        self.pump_on = True
+
+    def stop_pumping(self):
+        GPIO.output(self.pump_pin_number, 0)
+        self.pump_on = False
+
+    def pump_is_pumping(self):
+        return self.pump_on
