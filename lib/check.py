2a3e297ad0441f9e3b571559bd7c2fc1a60f8b13
Marie Yau <marie.dohnalova21@gmail.com>
2020-02-03 00:42:30 -0800

    added module for checking input

---
+++ lib/check.py
@@ -0,0 +1,8 @@
+# TODO: Implement function that checks if an integer is physical possible GPIO
+def is_valid_GPIO_pin_number(pin_number, mode):
+    pass
+
+def is_valid_bag_number(bag_number):
+    pass
+
+def
