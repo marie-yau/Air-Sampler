fe1e7eb26e4268a6e877e4d6a17b32001d07d780
Marie Yau <marie@ip-192-168-2-175.us-west-2.compute.internal>
2020-05-31 03:22:14 -0700

    Changed handling of uncaught exceptions

--- lib/logger.py
+++ lib/logger.py
@@ -1,11 +1,14 @@
 """
 Package for logging information to the log file.
 """
+import usb_drive
+import diode
 
 import sys
 import logging
 import settings
 import linecache
+import time
 
 def log_uncaught_exception(exception_type, exception_object, traceback):
     """
@@ -18,15 +21,30 @@ def log_uncaught_exception(exception_type, exception_object, traceback):
     filename = traceback.tb_frame.f_code.co_filename
     linecache.checkcache(filename)
     line = linecache.getline(filename, traceback.tb_lineno, traceback.tb_frame.f_globals)
-    logging.getLogger().exception('Exception {} raised in file ({}, line {} "{}"): {}'.format(exception_type,
+    logger = logging.getLogger("main logger")
+    logger.exception('Exception {} raised in file ({}, line {} "{}"): {}'.format(exception_type,
                                                                                               filename,
                                                                                               traceback.tb_lineno,
                                                                                               line.strip(),
                                                                                               exception_object))
     # set all GPIOs to output and turn them off
     settings.reset_gpio_pins()
-    settings.turn_Pi_off()
 
+    usb = USB_drive(logger)
+    # todo - When there is an error in config file, I can’t get the diode GPIO number and numbering mode.
+    #  Should I hard code it to create the diode object?
+    diode = Diode(18,
+                  "BCM",
+                  logger)
+    while True:
+        # if the usb was reinserted, restart the program
+        if usb.was_reinserted():
+            os.execl(sys.executable, sys.executable, *sys.argv)
+        # blink diode
+        diode.turn_diode_on()
+        time.sleep(0.5)
+        diode.turn_diode_off()
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     sys.excepthook = log_uncaught_exception
