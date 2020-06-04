173ecd1f9a449c006e1c5859ef0f17a805b37bbc
Marie Yau <marie@ip-192-168-2-175.us-west-2.compute.internal>
2020-06-04 09:17:29 -0700

    Fixed formatting

--- lib/hardware_errors.py
+++ lib/hardware_errors.py
@@ -2,6 +2,7 @@
 Custom errors and exceptions that are raised when there is a problem with hardware configuration
 """
 
+
 class HardwareConfigurationFileError(Exception):
     """
     Error that is raised when there is a problem with a hardware configuration file
@@ -13,6 +14,7 @@ class HardwareConfigurationFileError(Exception):
     def __str__(self):
         return "Invalid hardware configuration file ({}) - {}".format(self.file_path, self.message)
 
+
 class HardwareConfigurationError(Exception):
     """
     Error that is raised when there is a problem with hardware configuration
@@ -22,6 +24,7 @@ class HardwareConfigurationError(Exception):
 
     def __str__(self):
         return "Invalid hardware configuration - {}".format(self.message)
+
 
 if __name__ == "__main__":
     raise HardwareConfigurationFileError("/pi/home/config.txt", "Diode pin number is not specified.")
